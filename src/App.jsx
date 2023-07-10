7340f5e7e86616e02bd566a0bb9553852619b2b5
Vinicius Evangelista <viniciusevangelista088@gmail.comgit config --global user.name Vinni>
2023-07-10 16:18:59 -0300

    Item Cadastro OK

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,33 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom"
-import styled from "styled-components"
-import HomePage from "./pages/HomePage"
-import SignInPage from "./pages/SignInPage"
-import SignUpPage from "./pages/SignUpPage"
-import TransactionsPage from "./pages/TransactionPage"
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import styled from "styled-components";
+import HomePage from "./pages/HomePage";
+import SignInPage from "./pages/SignInPage";
+import SignUpPage from "./pages/SignUpPage";
+import TransactionsPage from "./pages/TransactionPage";
+import { createContext, useState } from "react";
+
+export const UserContext = createContext();
 
 export default function App() {
+  let [UserData, SetUserData] = useState(null);
+
   return (
-    <PagesContainer>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<SignInPage />} />
-          <Route path="/cadastro" element={<SignUpPage />} />
-          <Route path="/home" element={<HomePage />} />
-          <Route path="/nova-transacao/:tipo" element={<TransactionsPage />} />
-        </Routes>
-      </BrowserRouter>
-    </PagesContainer>
-  )
+    <UserContext.Provider value={{UserData, SetUserData}}>
+      <PagesContainer>
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<SignInPage />} />
+            <Route path="/cadastro" element={<SignUpPage />} />
+            <Route path="/home" element={<HomePage />} />
+            <Route
+              path="/nova-transacao/:tipo"
+              element={<TransactionsPage />}
+            />
+          </Routes>
+        </BrowserRouter>
+      </PagesContainer>
+    </UserContext.Provider>
+  );
 }
 
 const PagesContainer = styled.main`
@@ -25,4 +35,4 @@ const PagesContainer = styled.main`
   width: calc(100vw - 50px);
   max-height: 100vh;
   padding: 25px;
-`
+`;
--- src/pages/SignUpPage.jsx
+++ src/pages/SignUpPage.jsx
@@ -1,24 +1,82 @@
-import { Link } from "react-router-dom"
-import styled from "styled-components"
-import MyWalletLogo from "../components/MyWalletLogo"
+import axios from "axios";
+import { useState } from "react";
+import styled from "styled-components";
+import { Link, useNavigate } from "react-router-dom";
+import MyWalletLogo from "../components/MyWalletLogo";
 
 export default function SignUpPage() {
+  let [nome, setNome] = useState("");
+  let [email, setEmail] = useState("");
+  let [senha, setSenha] = useState("");
+  let [senha2, setSenha2] = useState("");
+  const url = import.meta.env.VITE_API_URL;
+  const navigate = useNavigate();
+
+  function Cadastro(e) {
+    e.preventDefault();
+    requisição();
+  }
+
+  function requisição() {
+    if (senha !== senha2) {
+      return alert("As senhas digitadas não coincidem.");
+    }
+
+    let novoCadastro = {
+      nome,
+      email,
+      senha,
+    };
+    const post = axios.post(`${url}/cadastro`, novoCadastro);
+    post.then(() => navigate("/"));
+    post.catch((resposta) => {
+      alert(
+        `Houve um problema com seu cadastro: ${resposta.response.data}`
+      );
+      console.log(resposta);
+    });
+    //console.log(novoCadastro)
+  }
   return (
     <SingUpContainer>
-      <form>
+      <form onSubmit={Cadastro}>
         <MyWalletLogo />
-        <input placeholder="Nome" type="text" />
-        <input placeholder="E-mail" type="email" />
-        <input placeholder="Senha" type="password" autocomplete="new-password" />
-        <input placeholder="Confirme a senha" type="password" autocomplete="new-password" />
-        <button>Cadastrar</button>
+        <input
+          placeholder="Nome"
+          type="text"
+          value={nome}
+          onChange={(e) => setNome(e.target.value)}
+        />
+
+        <input
+          placeholder="E-mail"
+          type="text"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+
+        <input
+          placeholder="Senha"
+          type="password"
+          autoComplete="new-password"
+          value={senha}
+          onChange={(e) => setSenha(e.target.value)}
+        />
+
+        <input
+          placeholder="Confirme a senha"
+          type="password"
+          autoComplete="new-password"
+          value={senha2}
+          onChange={(e) => setSenha2(e.target.value)}
+        />
+
+        <button type="submit">Cadastrar</button>
       </form>
 
-      <Link>
-        Já tem uma conta? Entre agora!
-      </Link>
+      <Link to={"/"}>Já tem uma conta? Entre agora!</Link>
     </SingUpContainer>
-  )
+  );
 }
 
 const SingUpContainer = styled.section`
@@ -27,4 +85,4 @@ const SingUpContainer = styled.section`
   flex-direction: column;
   justify-content: center;
   align-items: center;
-`
+`;
